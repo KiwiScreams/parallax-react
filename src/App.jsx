733e2585c5456eecc65cad605f076aedc310140f
KiwiScreams <screamskiwi3@gmail.com>
2024-08-05 20:25:55 +0400

    register

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Header from "./components/shared/header/Header";
-import Footer from "./components/shared/footer/Footer";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<Register />}></Route>
       </Routes>
     </>
   );
