db6e239a2e24cb299ab113177263a1d785256e6c
KiwiScreams <screamskiwi3@gmail.com>
2024-08-05 18:19:05 +0400

    faq

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -1,27 +1,36 @@
 import "./Faq.css"
 const FAQ = () => {
+    const faqData = 
+    [
+            {
+                title: "Terrifying Atmosphere:",
+                description: "Step into a world shrouded in darkness and mystery, where every corner holds unseen dangers and chilling surprises."
+              },
+              {
+                title: "Immersive Gameplay:",
+                description: "Engage in gameplay designed to keep you on the edge of your seat. Solve intricate puzzles, uncover the truth behind eerie mysteries, and navigate through spine-tingling environments."
+              },
+              {
+                title: "Thrilling Storyline:",
+                description: "Delve into a gripping narrative that unfolds with every twist and turn. Uncover the secrets buried within the game's narrative and confront the horrors that lurk in the shadows."
+              },
+              {
+                title: "Feedback Matters:",
+                description: "Your feedback is invaluable to us! Help shape the final experience by sharing your thoughts on gameplay mechanics, storyline progression, atmosphere, and overall immersion."
+              }
+    ]
     return ( 
         <>
         <div className="faq-container">
             <h2>What to Expect:</h2>
             <div className="faq-content flex">
-                <div className="faq">
-                    <h3>Terrifying Atmosphere:</h3>
-                    <p>Step into a world shrouded in darkness and mystery, where every corner holds unseen dangers and chilling surprises.</p>
-                </div>
-                <div className="faq">
-                    <h3>Terrifying Atmosphere:</h3>
-                    <p>Step into a world shrouded in darkness and mystery, where every corner holds unseen dangers and chilling surprises.</p>
-                </div>
-                <div className="faq">
-                    <h3>Terrifying Atmosphere:</h3>
-                    <p>Step into a world shrouded in darkness and mystery, where every corner holds unseen dangers and chilling surprises.</p>
-                </div>
-                <div className="faq">
-                    <h3>Terrifying Atmosphere:</h3>
-                    <p>Step into a world shrouded in darkness and mystery, where every corner holds unseen dangers and chilling surprises.</p>
-                </div>
-            </div>
+        {faqData.map((faq, index) => (
+          <div key={index} className="faq">
+            <h3>{faq.title}</h3>
+            <p>{faq.description}</p>
+          </div>
+        ))}
+      </div>
         </div>
         </>
      );
