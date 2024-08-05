5c4140fdd9450690e28ccc532c87d3bd8975753a
KiwiScreams <screamskiwi3@gmail.com>
2024-08-05 18:02:11 +0400

    main

--- src/components/text/TextBlock.jsx
+++ src/components/text/TextBlock.jsx
@@ -1,9 +1,13 @@
 import "./TextBlock.css"
+import sun from "../../assets/images/sun.png"
 function TextBlock() {
     return (
         <>
-            <div class="text-container">
-                <p class="text">Your text is here</p>
+            <div class="main-container flex">
+                <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum accusantium reiciendis enim? Perspiciatis fugit modi unde nisi quisquam impedit, molestias quaerat adipisci amet sunt libero suscipit aperiam minus, commodi in!</p>
+                <div className="sun-img">
+                    <img src={sun} alt="" />
+                </div>
             </div>
         </>
     )
