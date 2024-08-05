77f5000b068176a29ef012f83404304abe3b11ce
KiwiScreams <screamskiwi3@gmail.com>
2024-08-05 18:11:44 +0400

    faq style

---
+++ src/components/faq/Faq.jsx
@@ -0,0 +1,30 @@
+import "./Faq.css"
+const FAQ = () => {
+    return ( 
+        <>
+        <div className="faq-container">
+            <h2>What to Expect:</h2>
+            <div className="faq-content flex">
+                <div className="faq">
+                    <h3>Terrifying Atmosphere:</h3>
+                    <p>Step into a world shrouded in darkness and mystery, where every corner holds unseen dangers and chilling surprises.</p>
+                </div>
+                <div className="faq">
+                    <h3>Terrifying Atmosphere:</h3>
+                    <p>Step into a world shrouded in darkness and mystery, where every corner holds unseen dangers and chilling surprises.</p>
+                </div>
+                <div className="faq">
+                    <h3>Terrifying Atmosphere:</h3>
+                    <p>Step into a world shrouded in darkness and mystery, where every corner holds unseen dangers and chilling surprises.</p>
+                </div>
+                <div className="faq">
+                    <h3>Terrifying Atmosphere:</h3>
+                    <p>Step into a world shrouded in darkness and mystery, where every corner holds unseen dangers and chilling surprises.</p>
+                </div>
+            </div>
+        </div>
+        </>
+     );
+}
+ 
+export default FAQ;
--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,41 +1,49 @@
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
-import background from "../../assets/images/montain1.png"
-import sun from "../../assets/images/montain2.png"
-import mountain from "../../assets/images/montain3.png"
-import ground from "../../assets/images/montain4.png"
-import "./Parallax.css"
-import TextBlock from '../text/TextBlock'
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import background from "../../assets/images/montain1.png";
+import sun from "../../assets/images/montain2.png";
+import mountain from "../../assets/images/montain3.png";
+import ground from "../../assets/images/montain4.png";
+import "./Parallax.css";
+import TextBlock from "../text/TextBlock";
+import FAQ from "../faq/Faq";
 function ParallaxComponent() {
-    return (
-        <>
-            <div className="app">
-                <Parallax pages={2} style={{ top: '0', left: '0' }} className="animation">
-                    <ParallaxLayer offset={0} speed={0.5}>
-                        <div className='animation_layer parallax' id='background'>
-                            <img src={background} alt="mountain" />
-                        </div>
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={-0.08}>
-                        <div className='animation_layer parallax' id='sun'>
-                            <img src={sun} alt="sun" />
-                        </div>
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={0.35}>
-                        <div className='animation_layer parallax' id='mountain'>
-                            <img src={mountain} alt="mountain" />
-                        </div>
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={0.4}>
-                        <div className='animation_layer parallax' id='earth'>
-                            <img src={ground} alt="ground" />
-                        </div>
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={1} speed={0.7}>
-                        <TextBlock />
-                    </ParallaxLayer>
-                </Parallax>
+  return (
+    <>
+      <div className="app">
+        <Parallax
+          pages={3}
+          style={{ top: "0", left: "0" }}
+          className="animation"
+        >
+          <ParallaxLayer offset={0} speed={0.5}>
+            <div className="animation_layer parallax" id="background">
+              <img src={background} alt="mountain" />
             </div>
-        </>
-    )
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={-0.08}>
+            <div className="animation_layer parallax" id="sun">
+              <img src={sun} alt="sun" />
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={0.35}>
+            <div className="animation_layer parallax" id="mountain">
+              <img src={mountain} alt="mountain" />
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={0.4}>
+            <div className="animation_layer parallax" id="earth">
+              <img src={ground} alt="ground" />
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={1} speed={0.7}>
+            <TextBlock />
+          </ParallaxLayer>
+          <ParallaxLayer offset={2} speed={0.2}>
+            <FAQ />
+          </ParallaxLayer>
+        </Parallax>
+      </div>
+    </>
+  );
 }
-export default ParallaxComponent
+export default ParallaxComponent;
