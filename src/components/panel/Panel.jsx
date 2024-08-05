e357c62cef6ee864737d3db1b309b9da2264e47f
KiwiScreams <screamskiwi3@gmail.com>
2024-08-05 19:15:24 +0400

    panel

--- src/components/panel/Panel.jsx
+++ src/components/panel/Panel.jsx
@@ -1,16 +1,34 @@
-import "./Panel.css"
+import "./Panel.css";
 const Panel = () => {
-    return ( 
-        <>
-        <div className="panel-container flex">
-            <div className="panel">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eaque magni qui distinctio similique. Error harum nobis expedita voluptate, eveniet rem dolor aliquam, cumque odit iusto culpa architecto laboriosam amet.</p>
-            </div>
-            <div className="panel rotate">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eaque magni qui distinctio similique. Error harum nobis expedita voluptate, eveniet rem dolor aliquam, cumque odit iusto culpa architecto laboriosam amet.</p>
-            </div>
+  return (
+    <>
+      <div className="panel-container flex">
+        <div className="panel">
+          <div className="panel-content">
+            <h2>Join the Fear:</h2>
+            <p>
+              Are you ready to confront your fears and embark on a journey into
+              darkness? Sign up now and become part of an exclusive group of
+              testers who will help us shape the future of horror gaming. Dare
+              to test our new horror game—it's an experience you won't soon
+              forget.
+            </p>
+          </div>
         </div>
-        </>
-     );
-}
+        <div className="panel rotate">
+          <div className="panel-content">
+            <h2>Stay Updated:</h2>
+            <p>
+              Follow us on social media and subscribe to our newsletter for the
+              latest updates on the game's development, release dates, and more
+              spine-chilling announcements. Don't miss out on this opportunity
+              to be part of the terror—join us in our quest to redefine horror
+              gaming!
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default Panel;
